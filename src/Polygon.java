e508bd01fd7cc6184f5d5770e98852d7c86f2860
LucasW1234 <113708449+LucasW1234@users.noreply.github.com>
2023-02-09 09:41:16 -0800

    Finished what i could and understood.

--- src/Polygon.java
+++ src/Polygon.java
@@ -5,12 +5,16 @@
 //mutators
 //
 
+import java.awt.*;
+
 public class Polygon {
    private int NumSides;
    private double SideLength;
    private String ShapeType;
 
    private double perimeter;
+
+   private boolean valid;
 
 
    public Polygon(){
@@ -25,9 +29,11 @@ public class Polygon {
        ShapeType = type;
        if(NS <= 2){
            NS = 3;
+           valid = false;
        }
        if(SL <= 0){
            SL = 1;
+           valid = false;
        }
        if(NumSides == 3){
            ShapeType = "Triangle";
@@ -62,16 +68,36 @@ public class Polygon {
 
 //mutators
    public void calcPerimeter(double perimeter){
-       perimeter = NumSides*SideLength;
+       perimeter = Math.round(((SideLength*1000.0)*NumSides)/1000.0);
    }
 
 
 
     public void setNumSides(int NumSides) {
         this.NumSides = NumSides;
+        if(NumSides <= 2){
+            NumSides = 3;
+            valid = false;
+        }
     }
 
     public void setSideLength(double SideLength){
+
        this.SideLength = SideLength;
+       if(SideLength <= 0){
+           SideLength = 1;
+           valid = false;
+       }
     }
+
+
+    public String toString(){
+       if(valid = false){
+
+           return "The polygon you returned was invalid, it has reset to a equilateral triangle with the side lengths of zero";
+
+       }
+       return "Your shape is called " +ShapeType +"\nIt has a side length of " +SideLength +"\n As a " +ShapeType +" it has " +NumSides +" sides.";
+    }
+
 }
